d12ab2747d07c031d838a00d6daf2812b42a8261
qus0in <qus0in@gmail.com>
2025-04-22 14:08:37 +0900

    뭐지? 단기기억 상실증인가?

--- src/main/java/org/example/bootfileupload/controller/FileController.java
+++ src/main/java/org/example/bootfileupload/controller/FileController.java
@@ -43,10 +43,10 @@ public class FileController {
         return ResponseEntity.ok(myFileRepository.findAll());
     }
 
-    @GetMapping("/{filename}")
-    public ResponseEntity<byte[]> file(@PathVariable String filename) {
+    @GetMapping("/{bucketName}/{filename}")
+    public ResponseEntity<byte[]> file(@PathVariable String bucketName, @PathVariable String filename) {
         try {
-            byte[] fileBytes = storageService.download(filename); // 버킷 -> 파일 이름 요청
+            byte[] fileBytes = storageService.download("%s/%s".formatted(bucketName, filename)); // 버킷 -> 파일 이름 요청
 
             // 페이지(템플릿)로 응답하지 않고, 데이터로 응답하겠다
             return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"").contentType(MediaType.APPLICATION_OCTET_STREAM).body(fileBytes);
