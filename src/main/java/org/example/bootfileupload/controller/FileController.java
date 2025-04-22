6e669902bcbd6203f48cbbc573fc83f4d3ed0cd9
qus0in <qus0in@gmail.com>
2025-04-22 12:48:30 +0900

    밥먹고 띄우자...

--- src/main/java/org/example/bootfileupload/controller/FileController.java
+++ src/main/java/org/example/bootfileupload/controller/FileController.java
@@ -1,19 +1,25 @@
 package org.example.bootfileupload.controller;
 
 import lombok.extern.java.Log;
+import org.example.bootfileupload.model.entity.MyFile;
+import org.example.bootfileupload.model.repository.MyFileRepository;
 import org.example.bootfileupload.service.StorageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/file")
 @Log
 public class FileController {
     private final StorageService storageService;
+    private final MyFileRepository myFileRepository;
 
-    public FileController(StorageService storageService) {
+    public FileController(StorageService storageService, MyFileRepository myFileRepository) {
         this.storageService = storageService;
+        this.myFileRepository = myFileRepository;
     }
 
     @PostMapping(consumes = "multipart/form-data")
@@ -22,7 +28,15 @@ public class FileController {
             log.info(file.getOriginalFilename());
             String dbFilename = storageService.upload(file);
             log.info(dbFilename);
+            MyFile myFile = new MyFile();
+            myFile.setFilename(dbFilename);
+            myFileRepository.save(myFile);
         }
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MyFile>> getAll() {
+        return ResponseEntity.ok(myFileRepository.findAll());
     }
 }
