4a8d96e190768f2033d2a4d679513340f5200be5
qus0in <qus0in@gmail.com>
2025-04-22 12:36:47 +0900

    무슨 문제가 있겠지...

--- src/main/java/org/example/bootfileupload/controller/FileController.java
+++ src/main/java/org/example/bootfileupload/controller/FileController.java
@@ -1,6 +1,7 @@
 package org.example.bootfileupload.controller;
 
 import lombok.extern.java.Log;
+import org.example.bootfileupload.service.StorageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +10,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/file")
 @Log
 public class FileController {
+    private final StorageService storageService;
+
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
     @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<Void> upload(@RequestParam("files") MultipartFile[] files) {
+    public ResponseEntity<Void> upload(@RequestParam("files") MultipartFile[] files) throws Exception {
         for (MultipartFile file : files) {
             log.info(file.getOriginalFilename());
+            String dbFilename = storageService.upload(file);
+            log.info(dbFilename);
         }
         return ResponseEntity.ok().build();
     }
